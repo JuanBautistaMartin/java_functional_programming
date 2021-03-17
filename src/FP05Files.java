c25261301a251e89d74b2a5072a6db5d484d7fb1
juanBautista <juanbautista19@gmail.com>
2021-03-17 13:39:13 +0100

    no message

---
+++ src/FP05Files.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+/**
+ * Print a list of numbers
+ */
+public class FP05Files {
+
+    public static void main(String[] args) throws IOException {
+
+        Files.lines(Paths.get("file.txt"))
+                .forEach(System.out::println);
+
+        System.out.println("==========================================");
+
+        // Get unique words present in the file
+        Files.lines(Paths.get("file.txt"))
+                .map(str -> str.split(" "))
+                .flatMap(Arrays::stream)
+                .distinct()
+                .sorted()
+                .forEach(System.out::println);
+
+    }
+
+}
